02797dbbb931d7a556f66913b4acfb046cb3b4ed
niladrib <niladri.bora@gmail.com>
2023-12-27 14:06:23 -0800

    Added skeleton UI

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='ChargeFinder' component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+};
+
+export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, StyleSheet, Text} from "react-native";
+
+const HomeScreen = () => {
+    return <View>
+        <Text>Welcome to Charge Finder</Text>
+    </View>;
+};
+
+export default HomeScreen;
