ad04b820b78f73eb35e6f2a80bd0226a7aff217e
niladrib <niladri.bora@gmail.com>
2023-12-27 15:10:16 -0800

    retrieve charging points from open charge map API

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import {View, StyleSheet, Text} from "react-native";
+import { View, StyleSheet, Text, Button } from "react-native";
+import openChargeMap from "../api/openChargeMap";
 
 const HomeScreen = () => {
-    return <View>
-        <Text>Welcome to Charge Finder</Text>
-    </View>;
+  const apiKey = process.env.EXPO_PUBLIC_OPEN_CHARGE_MAP_API_KEY;
+  console.log(`key=${apiKey}`);
+  const findChargers = async () => {
+    openChargeMap
+      .get("/poi", {
+        params: {
+          output: "json",
+          maxresults: 100,
+          compact: true,
+          verbose: false,
+          distance: 10,
+          distanceunit: "miles",
+          latitude: 39.19005300900668,
+          longitude: -120.96667310680074,
+        },
+      })
+      .then((response): void => {
+        console.log(`Got response=${JSON.stringify(response.data)}`);
+      })
+      .catch((err): void => {
+        console.log(`Got error when getting POIs=${err}`);
+      });
+  };
+  return (
+    <View>
+      <Button title="Find Chargers Near Me" onPress={findChargers} />
+    </View>
+  );
 };
 
 export default HomeScreen;
